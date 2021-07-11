0dda9b3bfd551d86ea990062bcfc89d00237aa1e
Carrie <carr1eg@hotmail.com>
2021-07-10 21:59:21 -0700

    exercises 2.18 - 2.20  added error msgs

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setFilter] = useState('');
   const [reloadPerson, setReloadPerson] = useState(false);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     axios
@@ -25,7 +26,35 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault();
     if (persons.find(e => e.name === name)) {
-      window.alert(`${name} is already added to phonebook`);
+      window.confirm(`${name} is already added to phonebook,replace the old number with a new one?`);
+      let id = persons.filter(e => e.name === name)[0].id;
+      const person = {
+        name: name,
+        number: newNumber
+      }
+      axios
+        .put(`http://localhost:3001/persons/${id}`, person)
+        .then(response => {
+          setPersons(persons.map(person => person.id !== id ? person : response.data));
+          setName('');
+          setNewNumber('');
+          setMessage(
+            `Updated ${person.name}`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
+        .catch(error => {
+          setMessage(
+            `${person.name} is already deleted from the database :(`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+          setName('');
+          setNewNumber("");
+          })
     } 
     else {
       const person = {
@@ -35,12 +64,18 @@ const App = () => {
       axios
         .post('http://localhost:3001/persons', person)
         .then(response => {
-          console.log(response);
           setPersons(persons.concat(person));
           setName('');
           setNewNumber("");
+          setMessage(
+            `Added ${person.name}`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
         }
           )
+        
     }
   }
   const handleNumberChange = (event) => {
@@ -52,18 +87,25 @@ const App = () => {
   const handleFilter = (event) => {
     setFilter(event.target.value);
   }
-  const handleDelButton = (person) => {
-    if(window.confirm(`Delete ${person.name}?`)){
-console.log('person :>> ', person);
+  const handleDelButton = (event) => {
+    if(window.confirm(`Delete ${event.target.value}?`)){
       axios
-           .delete(`http://localhost:3001/persons/${person.id}`)
+           .delete(`http://localhost:3001/persons/${event.target.dataset.mssg}`)
            .then(response => {
              console.log('response :>> ', response)
              setReloadPerson(true);
 
            })
-           .catch(err => console.log(err))
-  
+           .catch(error => {
+            setMessage(
+              `${event.target.value} is already deleted from the database :(`
+            )
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+            setName('');
+            setNewNumber("");
+            })  
 }}
 
   const personToShow = !newFilter
@@ -76,6 +118,7 @@ console.log('person :>> ', person);
 return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={message}/>
       <Filter handleFilter={handleFilter} newFilter={newFilter} />
       <h3>add a new</h3>
       <PersonForm addPerson={addPerson} name={name} handleNameChange={handleNameChange}
@@ -92,6 +135,15 @@ return (
   )
 }
 
-
+const Notification = ({notification}) => {
+  if(notification === null){
+    return null;
+  }
+  return(
+    <div className="notification">
+      {notification}
+    </div>
+  )
+}
 
 export default App
--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -1,16 +1,9 @@
 
 export const Phonebook = ({person, handleDelButton, key}) => { 
-  const clickHandler = () => {
-    handleDelButton(person);
-  }
-
-
-
-
   return(
     <div>
       {person.name} {person.number} 
-      <button value={person.name}  onClick={clickHandler}>delete</button>
+      <button value={person.name} data-mssg={person.id} onClick={handleDelButton}>delete</button>
     </div>
   )
 
